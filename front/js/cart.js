f9186dd2f3e8089f06a98fa994d84e22303dcaf4
Fred aka <100697741+fredgAKA@users.noreply.github.com>
2022-08-10 21:06:17 +0200

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -169,3 +169,286 @@ function deleteProduct() {
     }
 }
 deleteProduct();
+
+//Instauration formulaire avec Regex
+function getForm() {
+    //Ajout des Regex
+    let form = document.querySelector(".cart__order__form");
+
+    //Création des expressions régulières
+    let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+    let charRegExp = new RegExp("^[a-zA-Z ,.'-]+$");
+    let addressRegExp = new RegExp("^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+");
+
+    //Ecoute de la modif du prénom dans le formulaire
+    form.firstName.addEventListener('change', function() {
+        validFirstName(this);
+    });
+
+    // Ecoute de la modif du Nom de famille dans le formulaire
+    form.lastName.addEventListener('change', function() {
+        validLastName(this);
+    });
+
+    //Ecoute de la modif de l'adresse dans le formulaire
+    form.address.addEventListener('change', function() {
+        valideAddress(this);
+    });
+
+    //Ecoute de la modif de la ville dans le formulaire
+    form.city.addEventListener('change', function() {
+        validCity(this);
+    });
+
+    //Ecoute de la modif de l'email dans le formulaire
+    form.email.addEventListener('change', function() {
+        valideEmail(this);
+    });
+
+    //Validation du prénom
+    const validFirstName = function(inputFirstName) {
+        let firstNameErrorMsg = inputFirstName.nextElementSibling;
+
+        if (charRegExp.test(inputFirstName.value)) {
+            firstNameErrorMsg.innerHTML = '';
+        } else {
+            firstNameErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+
+    //validation du nom
+    const validLastName = function(inputLastName) {
+        let lastNameErrorMsg = inputLastName.nextElementSibling;
+
+        if (charRegExp.test(inputLastName.value)) {
+            lastNameErrorMsg.innerHTML = '';
+        } else {
+            lastNameErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+
+    //validation de l'adresse
+    const validAddress = function(inputAddress) {
+        let addressErrorMsg = inputAddress.nextElementSibling;
+
+        if (addressRegExp.test(inputAddress.value)) {
+            addressErrorMsg.innerHTML = '';
+        } else {
+            addressErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+
+    //validation de la ville
+    const validCity = function(inputCity) {
+        let cityErrorMsg = inputCity.nextElementSibling;
+
+        if (charRegExp.test(inputCity.value)) {
+            cityErrorMsg.innerHTML = '';
+        } else {
+            cityErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+
+    //validation de l'email
+    const validEmail = function(inputEmail) {
+        let emailErrorMsg = inputEmail.nextElementSibling;
+
+        if (emailRegExp.test(inputEmail.value)) {
+            emailErrorMsg.innerHTML = '';
+        } else {
+            emailErrorMsg.innerHTML = 'Veuillez renseigner votre email.';
+        }
+    };
+}
+getForm();
+
+// Le formulaire
+
+// Selection du bouton valider
+const btnValidate = document.querySelector('#order');
+
+// Ecoute du bouton valider sur le click pour pouvoir valider le formulaire
+btnValidate.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let contact = {
+        firstName: document.querySelector("#firstName").value,
+        lastName: document.querySelector("#lastName").value,
+        address: document.querySelector("#address").value,
+        city: document.querySelector("#city").value,
+        email: document.querySelector("#email").value,
+    };
+
+    console.log(contact);
+
+    //Gestion formulaire
+
+    //regex pour le contrôle des champs prénom, nom et ville
+
+    const regExPrenomNomVille = (value) => {
+        return /^[A-Z][A-Za-z\é\è\ê\-]+$/.test(value);
+      };
+    
+      // Regex pour le contrôle du champ Adresse
+      const regExAdresse = (value) => {
+        return /^[a-zA-Z0-9.,-_ ]{5,50}[ ]{0,2}$/.test(value);
+      };
+    
+      // Regex pour le contrôle du champ Email
+      const regExEmail = (value) => {
+        return /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(
+          value
+        );
+      };
+    
+      // Fonctions de contrôle du champ Prénom:
+      function firstNameControl() {
+        const prenom = contact.firstName;
+        let inputFirstName = document.querySelector("#firstName");
+        if (regExPrenomNomVille(prenom)) {
+          inputFirstName.style.backgroundColor = "green";
+    
+          document.querySelector("#firstNameErrorMsg").textContent = "";
+          return true;
+        } else {
+          inputFirstName.style.backgroundColor = "#FF6F61";
+    
+          document.querySelector("#firstNameErrorMsg").textContent =
+            "Champ Prénom de formulaire invalide, ex: Paul";
+          return false;
+        }
+      }
+    
+      // Fonctions de contrôle du champ Nom:
+      function lastNameControl() {
+        const nom = contact.lastName;
+        let inputLastName = document.querySelector("#lastName");
+        if (regExPrenomNomVille(nom)) {
+          inputLastName.style.backgroundColor = "green";
+    
+          document.querySelector("#lastNameErrorMsg").textContent = "";
+          return true;
+        } else {
+          inputLastName.style.backgroundColor = "#FF6F61";
+    
+          document.querySelector("#lastNameErrorMsg").textContent =
+            "Champ Nom de formulaire invalide, ex: Durand";
+          return false;
+        }
+      }
+    
+      // Fonctions de contrôle du champ Adresse:
+      function addressControl() {
+        const adresse = contact.address;
+        let inputAddress = document.querySelector("#address");
+        if (regExAdresse(adresse)) {
+          inputAddress.style.backgroundColor = "green";
+    
+          document.querySelector("#addressErrorMsg").textContent = "";
+          return true;
+        } else {
+          inputAddress.style.backgroundColor = "#FF6F61";
+    
+          document.querySelector("#addressErrorMsg").textContent =
+            "Champ Adresse de formulaire invalide, ex: 50 rue de la paix";
+          return false;
+        }
+      }
+    
+      // Fonctions de contrôle du champ Ville:
+      function cityControl() {
+        const ville = contact.city;
+        let inputCity = document.querySelector("#city");
+        if (regExPrenomNomVille(ville)) {
+          inputCity.style.backgroundColor = "green";
+    
+          document.querySelector("#cityErrorMsg").textContent = "";
+          return true;
+        } else {
+          inputCity.style.backgroundColor = "#FF6F61";
+    
+          document.querySelector("#cityErrorMsg").textContent =
+            "Champ Ville de formulaire invalide, ex: Paris";
+          return false;
+        }
+      }
+    
+      // Fonctions de contrôle du champ Email:
+      function mailControl() {
+        const courriel = contact.email;
+        let inputMail = document.querySelector("#email");
+        if (regExEmail(courriel)) {
+          inputMail.style.backgroundColor = "green";
+    
+          document.querySelector("#emailErrorMsg").textContent = "";
+          return true;
+        } else {
+          inputMail.style.backgroundColor = "#FF6F61";
+    
+          document.querySelector("#emailErrorMsg").textContent =
+            "Champ Email de formulaire invalide, ex: example@contact.fr";
+          return false;
+        }
+      }
+    
+      // Contrôle validité formulaire avant de l'envoyer dans le local storage
+      if (
+        firstNameControl() &&
+        lastNameControl() &&
+        addressControl() &&
+        cityControl() &&
+        mailControl()
+      ) {
+        // Enregistrer le formulaire dans le local storage
+        localStorage.setItem("contact", JSON.stringify(contact));
+    
+        document.querySelector("#order").value =
+          "Merci pour votre commande !";
+        sendToServer();
+      } else {
+        error("Veuillez bien remplir le formulaire");
+      }
+    
+      /* FIN GESTION DU FORMULAIRE */
+    
+      /* REQUÊTE DU SERVEUR ET POST DES DONNÉES */
+      function sendToServer() {
+        const sendToServer = fetch("http://localhost:3000/api/products/order", {
+          method: "POST",
+          body: JSON.stringify({ contact, products }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          // Récupération et stockage de la réponse de l'API (orderId)
+          .then((response) => {
+            return response.json();
+          })
+          .then((server) => {
+            orderId = server.orderId;
+            console.log(orderId);
+          });
+    
+        // Si l'orderId a bien été récupéré, on redirige l'utilisateur vers la page de Confirmation
+        if (orderId != "") {
+          location.href = "confirmation.html?id=" + orderId;
+        }
+      }
+    });
+    
+    /* FIN REQUÊTE DU SERVEUR ET POST DES DONNÉES */
+    
+    // Maintenir le contenu du localStorage dans le champs du formulaire
+    
+    let dataFormulaire = JSON.parse(localStorage.getItem("contact"));
+    
+    console.log(dataFormulaire);
+    if (dataFormulaire) {
+      document.querySelector("#firstName").value = dataFormulaire.firstName;
+      document.querySelector("#lastName").value = dataFormulaire.lastName;
+      document.querySelector("#address").value = dataFormulaire.address;
+      document.querySelector("#city").value = dataFormulaire.city;
+      document.querySelector("#email").value = dataFormulaire.email;
+    } else {
+      console.log("Le formulaire est vide");
+    }
